079341edf2517d04567d2b47e2d2fe449a494215
Victoria Loskutova <viktoriia.loskutova@gmail.com>
2023-06-07 14:21:48 +0700

    first commit

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,5 @@
-/* eslint-disable */
+import { getJestProjects } from '@nx/jest';
+
 export default {
-  displayName: 'tgbot-useroftheday',
-  preset: './jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: './coverage/tgbot-useroftheday',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+  projects: getJestProjects(),
 };
---
+++ user-of-the-day/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'user-of-the-day',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/user-of-the-day',
+};
